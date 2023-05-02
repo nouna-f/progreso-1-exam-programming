d7fd98b1017721ae6f94edd2b8a05ed0fae87f88
nouna-f <nourine.founouni@udla.edu.ec>
2023-05-02 12:54:31 -0500

    Ultimo commit con el codigo acabdo y para que se suma 1 al contador cuando el usario ingrese un negativo.

--- Progreso1examen.c
+++ Progreso1examen.c
@@ -4,8 +4,19 @@ int main() {
     int num, count_neg = 0; // Declaré num y count_neg, inicializando count_neg en 0
     // Ulitice num para almacenar los números introducidos por el usuario y count_neg es un contador para contar cuantos números negativos hay
     
-    while (1) { // Utilice while infinito, para parar el programa cuando se introduzca un 0
+    while (1) { // Utilice while infinito, para finalizar/parar el programa cuando se introduzca un 0
         printf("Introduce un numero (0 para salir): "); // Pido al usuario que introduzca un número
         scanf("%d", &num); // El programa lee el número introducido y se almacena en num
         
+        if (num == 0) { // Si el número introducido por el usuario es 0, el programa sale del bucle while
+            break;
+        }
         
+        count_neg += (num < 0); // Si un número es negativo, se suma 1 al contador count_neg. 
+        // Puse (num < 0) para que se suma 1 al contador solo si el número es negativo.
+    }
+    
+    printf("Cantidad de numeros negativos introducidos: %d\n", count_neg); // Aqui el programa muestra el usuario la cantidad de números negativos introducidos
+    
+    return 0; // Aqui el programa retorna 0 para indicar que el programa ha finalizado.
+}
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    int num, count_neg = 0;
-    // En este caso, no es necesario inicializar count_neg en cero
-    // Utilizo count_neg para contar numeros negativos
+    int num, count_neg = 0; // Declaré num y count_neg, inicializando count_neg en 0
+    // Ulitice num para almacenar los números introducidos por el usuario y count_neg es un contador para contar cuantos números negativos hay
     
-    do {  // Se utilice do-while para garantizar que se lea al menos un número antes de verificar si el usuario desea salir
-        printf("Introduce un numero (0 para salir): ");
-        scanf("%d", &num);
+    while (1) { // Utilice while infinito, para finalizar/parar el programa cuando se introduzca un 0
+        printf("Introduce un numero (0 para salir): "); // Pido al usuario que introduzca un número
+        scanf("%d", &num); // El programa lee el número introducido y se almacena en num
         
-        if (num < 0) {
-            count_neg++;
+        if (num == 0) { // Si el número introducido por el usuario es 0, el programa sale del bucle while
+            break;
         }
-    } while (num != 0);  // El bucle se repetirá mientras el usuario no introduzca el número cero
+        
+        count_neg += (num < 0); // Si un número es negativo, se suma 1 al contador count_neg. 
+        // Puse (num < 0) para que se suma 1 al contador solo si el número es negativo.
+    }
     
-    printf("Cantidad de numeros negativos introducidos: %d\n", count_neg);
+    printf("Cantidad de numeros negativos introducidos: %d\n", count_neg); // Aqui el programa muestra el usuario la cantidad de números negativos introducidos
     
-    return 0;
+    return 0; // Aqui el programa retorna 0 para indicar que el programa ha finalizado.
 }
